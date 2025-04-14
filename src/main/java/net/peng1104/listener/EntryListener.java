462a67c58fc9f7c985eaeb73aeedd884b6b1baef
Peng1104 <admin@peng1104.net>
2025-04-14 20:35:53 -0300

    Aula 4
    Took 4 minutes

--- src/main/java/net/peng1104/listener/EntryListener.java
+++ src/main/java/net/peng1104/listener/EntryListener.java
@@ -7,7 +7,9 @@ import net.peng1104.profiles.Profile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.jetbrains.annotations.NotNull;
 
 @RequiredArgsConstructor
@@ -28,6 +30,21 @@ public class EntryListener implements Listener {
         }
         else {
             profile.sendMessage("&c&lQue pena, você não pode ver a mensagem de entrada.");
+        }
+    }
+    
+    @EventHandler
+    private void onPlayerDie(PlayerDeathEvent event) {
+    
+    }
+    
+    @EventHandler
+    private void onPlayerMove(@NotNull PlayerMoveEvent event) {
+        Profile profile = pl.getProfileManager().getProfile(event.getPlayer().getUniqueId());
+        
+        if (profile.hasPermission("insper.code.congelar")) {
+            event.getPlayer().sendMessage("Você não pode se mover!");
+            event.setCancelled(true); // Congela o jogador no lugar
         }
     }
 }
